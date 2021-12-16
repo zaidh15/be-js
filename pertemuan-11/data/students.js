036d34c472ce48a33072988018436358359ef5c9
ZaidHilmi <56177261+zaidh15@users.noreply.github.com>
2021-12-16 10:16:21 +0700

    Add: task pertemuan 11

---
+++ pertemuan-11/data/students.js
@@ -0,0 +1,12 @@
+// TODO 1: Buat data students
+// code here
+students = [
+    "Fazri",
+    "Wangsit",
+    "Zaid"
+];
+
+
+// TODO 2: export data students
+// code here
+module.exports = students;
